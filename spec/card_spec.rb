f3dc503fc441b59a8d98fc3cc7edde44ba46be91
Vicente Santos <vicentedpsantos@gmail.com>
2019-04-28 15:17:27 -0300

    Added type test to card

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,8 +1,13 @@
 class Card
-  attr_reader :type
+  attr_reader :rank, :suite
 
-  def initialize(type)
-    @type = type
+  def initialize(rank, suite)
+    @rank = rank
+    @suite = suite
+  end
+
+  def type
+    "#{rank} of #{suite}"
   end
 end
 
@@ -10,10 +15,16 @@ end
 RSpec.describe Card do
   it 'has a rank' do
     card = Card.new('Ace', 'Spades')
-    expect(card.rank).to eq('Two')
+    expect(card.rank).to eq('Ace')
   end
 
   it 'has a suite' do
     card = Card.new('Ace', 'Spades')
+    expect(card.suite).to eq('Spades')
+  end
+
+  it 'has a type' do
+    card = Card.new('Ace', 'Spades')
+    expect(card.type).to eq('Ace of Spades')
   end
 end
