5abe6f29047716b59f66531974190bf23be05b4c
Vicente Santos <vicentedpsantos@gmail.com>
2019-04-28 16:37:27 -0300

    Overwriting let statement from preceding block

---
+++ spec/overwriting_let_spec.rb
@@ -0,0 +1,25 @@
+class ProgrammingLanguage
+  attr_reader :name
+
+  def initialize(name = 'Ruby')
+    @name = name
+  end
+end
+
+RSpec.describe ProgrammingLanguage do
+  let(:language) { ProgrammingLanguage.new('Python') }
+
+  context 'with argument' do
+    it 'should store the name of the language' do
+      expect(language.name).to eq('Python')
+    end
+  end
+
+  context 'with no argument' do
+    let(:language) { ProgrammingLanguage.new }
+
+    it 'should default to Ruby as the name' do
+      expect(language.name).to eq('Ruby')
+    end
+  end
+end
