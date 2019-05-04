e7c3e50545b525330c562c90bbb8ac0e47590d37
Vicente Santos <vicentedpsantos@gmail.com>
2019-05-04 19:04:38 -0300

    Added be matcher examples

---
+++ spec/matcher_be_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe 'be matchers' do
+  it 'can test for truthiness' do
+    expect(true).to be_truthy
+    expect('hello').to be_truthy
+    expect(5).to be_truthy
+    expect(0).to be_truthy
+    expect(-1).to be_truthy
+    expect(3.13).to be_truthy
+    expect([]).to be_truthy
+    expect([1, 2]).to be_truthy
+    expect({}).to be_truthy
+    expect(:symbol).to be_truthy
+  end
+
+  it 'can test for falsiness' do
+    expect(false).to be_falsy
+    expect(nil).to be_falsy
+  end
+
+  it 'can test nil' do
+    expect(nil).to be_nil
+
+    my_hash = { a: 5 }
+    expect(my_hash[:b]).to be_nil
+  end
+end
